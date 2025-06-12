1c462e4b4b668a8a103a2193c6d514f1a44a7998
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-06-12 17:29:51 +0200

    Create ship class

---
+++ src/game/ship.js
@@ -0,0 +1,45 @@
+export default class Ship {
+    constructor(position, length, rotation) {
+        this.startPosition = position;
+        this.shipLength = length;
+        this.rotation = rotation;
+        // ship cells
+        this.cells = Array(this.shipLength).fill(false);
+    }
+
+    isHit(position) {
+        // checks if position is on ship
+        if (this.rotation === "vertical") {
+            if (position[1] < this.startPosition[1] + this.shipLength && position[1] >= this.startPosition[1]) {
+                if (position[0] === this.startPosition[0]) {
+                    return true;
+                }
+            }
+        }
+        if (position[0] < this.startPosition[0] + this.shipLength && position[0] >= this.startPosition[0]) {
+            if (position[1] === this.startPosition[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    hit(position) {
+        // hits the ship at position
+        let cellPosition = 0;
+        // get the cell that was hit
+        if (this.rotation === "vertical") {
+            cellPosition = this.startPosition[1] - position[1];
+        }
+        else {
+            cellPosition = this.startPosition[0] - position[0];
+        }
+        // mark the cell as hit
+        this.cells[cellPosition] = true;
+    }
+
+    isSunk() {
+        console.log("CELLS", this.cells)
+        return !this.cells.includes(false);
+    }
+}
