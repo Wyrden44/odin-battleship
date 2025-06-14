48e6d3ec3bf4611bf617f9c9969dfc71386d4264
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-06-14 20:03:58 +0200

    Create board displayer

---
+++ src/interface/boardDisplayer.js
@@ -0,0 +1,39 @@
+export default class BoardDisplayer {
+    static createBoard(boardArray) {
+        const boardContainer = document.createElement("div");
+
+        boardArray.forEach((row, rowIndex) => {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("board-row");
+            row.forEach((cell, colIndex) => {
+                const cellDiv = document.createElement("div");
+                cellDiv.classList.add("board-cell");
+                cellDiv.dataset.row = rowIndex;
+                cellDiv.dataset.col = colIndex;
+                rowDiv.appendChild(cellDiv);
+            });
+            boardContainer.appendChild(rowDiv);
+        });
+        return boardContainer;
+    }
+
+    static markShipCell(boardContainer, cellPos) {
+        const cell = boardContainer.querySelector(`.board-cell[data-row="${cellPos.row}"][data-col="${cellPos.col}"]`);
+        cell.classList.add("ship");
+    }
+
+    static markShipCell(boardContainer, cellPos) {
+        const cell = boardContainer.querySelector(`.board-cell[data-row="${cellPos.row}"][data-col="${cellPos.col}"]`);
+        cell.classList.add("ship-hit");
+    }
+
+    static markShipDestroyedCell(boardContainer, cellPos) {
+        const cell = boardContainer.querySelector(`.board-cell[data-row="${cellPos.row}"][data-col="${cellPos.col}"]`);
+        cell.classList.add("ship-destroyed");
+    }
+
+    static markMissCell(boardContainer, cellPos) {
+        const cell = boardContainer.querySelector(`.board-cell[data-row="${cellPos.row}"][data-col="${cellPos.col}"]`);
+        cell.classList.add("miss");
+    }
+}
